00f9b5eccdc174a813a6ba3ff689fcc1171a6882
Pizzayolow <ralliard.valentin@gmail.com>
2023-10-12 14:48:36 +0200

    setup all basics routes and base html

---
+++ src/webapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import WebappLogin, WebappHome, CreateOrder, EditOrder, DeleteOrder, Search, Dashboard
+
+app_name = 'webapp'
+
+urlpatterns = [
+    path('', WebappLogin.as_view(), name='login'),
+    path('home/', WebappHome.as_view(), name='home'),
+    path('create/', CreateOrder.as_view(), name='create'),
+    path('edit/', EditOrder.as_view(), name='edit'),
+    path('delete/', DeleteOrder.as_view(), name='delete'),
+    path('search/', Search.as_view(), name='search'),
+    path('dashboard/', Dashboard.as_view(), name='dashboard'),
+]
