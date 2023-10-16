6671ec5a0a71b7d6a5cab3f6cd0b97d25a424468
Descombes Simon <pro.descombes@gmail.com>
2023-10-16 15:11:11 +0200

    Add initial create form

--- src/order_nest/urls.py
+++ src/order_nest/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django_select2 import urls as django_select2_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("select2/", include("django_select2.urls")),
     path('', include('webapp.urls')),
 ]
