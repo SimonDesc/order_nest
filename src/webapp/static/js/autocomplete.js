95d04e23f97db51f695233aa820352957cce9ffe
Descombes Simon <pro.descombes@gmail.com>
2023-10-28 23:02:40 +0200

    improve the client search list

--- src/webapp/static/js/autocomplete.js
+++ src/webapp/static/js/autocomplete.js
@@ -2,6 +2,30 @@ $(function() {
     $("#id_last_name").autocomplete({
         source: "/get_clients/",
         minLength: 2,
+        open: function() {
+
+            // Styles et classes pour le conteneur du menu déroulant
+            $(".ui-autocomplete").addClass(
+                "absolute top-full left-0 z-[1000] min-w-[200px] max-w-[300px] " +
+                "py-2 border border-gray-300 rounded-lg shadow-lg bg-white"
+            );
+
+            // Fait en sorte que chaque élément de menu occupe toute la largeur disponible
+            $(".ui-menu-item").addClass("w-full");
+
+            // Styles pour le contenu de chaque élément de menu
+            $(".ui-menu-item-wrapper").addClass(
+                "w-full text-lg text-gray-900 py-2 px-4 " +
+                "hover:bg-gray-200 hover:text-black cursor-pointer"
+            );
+
+            // Fixe la largeur du conteneur du menu déroulant à 220px ou à une autre valeur que vous préférez
+            $(".ui-autocomplete").css("width", "220px");
+
+            // Supprime les éléments inaccessibles qui ne sont pas nécessaires
+            $(".ui-helper-hidden-accessible").remove();
+
+        },
         select: function(event, ui) {
             $('#id_first_name').val(ui.item.first_name);
             $('#id_last_name').val(ui.item.last_name);
@@ -10,6 +34,11 @@ $(function() {
             $('#id_mail').val(ui.item.mail);
             $('#id_customer').val(ui.item.id);
             return false;
+        },
+        response: function(event, ui) {
+            if (ui.content.length === 0) {
+                ui.content.push({ label: "Aucun résultat trouvé", value: "" });
+            }
         }
     });
 });
