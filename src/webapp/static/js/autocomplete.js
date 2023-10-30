8f0656f8383e04619f3d34e9b924981e46ed5914
Descombes Simon <pro.descombes@gmail.com>
2023-10-30 09:43:23 +0100

    add white bg for canvas

--- src/webapp/static/js/autocomplete.js
+++ src/webapp/static/js/autocomplete.js
@@ -4,25 +4,20 @@ $(function() {
         minLength: 2,
         open: function() {
 
-            // Styles et classes pour le conteneur du menu déroulant
             $(".ui-autocomplete").addClass(
                 "absolute top-full left-0 z-[1000] min-w-[200px] max-w-[300px] " +
                 "py-2 border border-gray-300 rounded-lg shadow-lg bg-white"
             );
 
-            // Fait en sorte que chaque élément de menu occupe toute la largeur disponible
             $(".ui-menu-item").addClass("w-full");
 
-            // Styles pour le contenu de chaque élément de menu
             $(".ui-menu-item-wrapper").addClass(
                 "w-full text-lg text-gray-900 py-2 px-4 " +
                 "hover:bg-gray-200 hover:text-black cursor-pointer"
             );
 
-            // Fixe la largeur du conteneur du menu déroulant à 220px ou à une autre valeur que vous préférez
             $(".ui-autocomplete").css("width", "220px");
 
-            // Supprime les éléments inaccessibles qui ne sont pas nécessaires
             $(".ui-helper-hidden-accessible").remove();
 
         },
