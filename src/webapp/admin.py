336fbc8d4619900edec6223f8e3f3ee8dd254f93
Pizzayolow <ralliard.valentin@gmail.com>
2023-11-01 14:28:42 +0100

    add colors and modify status and dynamics colors in home and dashboard

--- src/webapp/admin.py
+++ src/webapp/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Customer, Status, Product, Order, OrderHasProduct
+from .models import Customer, Product, Order, OrderHasProduct
 
 admin.site.register(Customer)
-admin.site.register(Status)
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(OrderHasProduct)
--- src/webapp/forms.py
+++ src/webapp/forms.py
@@ -8,11 +8,6 @@ class NewOrderForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(NewOrderForm, self).__init__(*args, **kwargs)
-        try:
-            default_status = Status.objects.first()
-        except Status.DoesNotExist:
-            default_status = None
-        self.fields['status'].initial = default_status
         tailwind_class = "resize-y block p-2.5 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'"
         for field_name, field in self.fields.items():
                 field.widget.attrs.update({'class': tailwind_class})
