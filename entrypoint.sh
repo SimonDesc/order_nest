3c03d92b39a6d405737ae23b6aed381045e0c69b
SimonDesc <124585760+SimonDesc@users.noreply.github.com>
2023-12-19 14:31:11 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd src
+
+pipenv run python manage.py migrate --no-input
+pipenv run python manage.py collectstatic --no-input
+pipenv run gunicorn order_nest.wsgi:application --bind 0.0.0.0:$PORT --access-logfile - -w 4 --timeout 600 --log-level debug
