c703dfee0306b89dabf1295dc175615cb0e7711a
Descombes Simon <pro.descombes@gmail.com>
2024-01-27 20:41:10 +0100

    refacto: optimise weight of pictures and show them in the order. also somes changes on cards.

--- src/webapp/static/js/picture.js
+++ src/webapp/static/js/picture.js
@@ -1,49 +1,111 @@
 $(document).ready(function () {
-	$("#photoCaptureInput").on("change", async function (event) {
+	$("#progressContainer").hide();
+	$("#progressText").hide();
+
+	$("#photoCaptureInput").on("change", function (event) {
+
+		$("#progressContainer").show();
+		$("#progressText").show();
+		$("#progressBar").width('0%')
+
 		const file = event.target.files[0];
+		// Si il n'y a pas de fichier
 		if (!file) {
-			return; // Gestion de l'absence de fichier
+			return;
 		}
 
-		let formData = new FormData();
-		formData.append("img", file);
-		const idOrder = $("#orderPk").val();
-		formData.append("orderId", idOrder);
-		const csrfToken = $("[name=csrfmiddlewaretoken]").val();
+		// If the size of the target image is too large (e.g., greater than 10 MB),
+		// you should disable this option to avoid an out - of - memory crash.
+		// see: https://github.com/fengyuanchen/compressorjs
+		const maxFileSize = 10 * 1024 * 1024;
+		if (file.size > maxFileSize) {
+			alert("La taille du fichier est trop grande.");
+			return;
+		}
 
-		try {
-			const response = await fetch(`/save_pictures/`, {
-				method: 'POST',
-				headers: {
-					'X-CSRFToken': csrfToken
-				},
-				body: formData
-			});
+		// Compression de l'image
+		new Compressor(file, {
+			quality: 0.4,
+			maxWidth: 1000,
+			success(result) {
+				let formData = new FormData();
+				formData.append("img", result, result.name);
+				sendPicture(formData);
+			},
+			error(err) {
+				console.log(err.message);
+			},
 
-			if (!response.ok) {
-				const errorResponse = await response.json();
-				let errorMessage = 'Erreur lors du traitement de la requête.';
-				if (errorResponse && errorResponse.message) {
-					errorMessage = errorResponse.message;
+		});
+
+		$("#progressBar").width('50%')
+		$("#progressNumber").text('50%')
+
+		// Envoi de l'image au serveur
+		async function sendPicture(formData) {
+			$("#progressBar").width('75%');
+			$("#progressNumber").text('75%')
+			// On envoi l'id de la commande
+			const idOrder = $("#orderPk").val();
+			formData.append("orderId", idOrder);
+
+			// On gère le csrf
+			const csrfToken = $("[name=csrfmiddlewaretoken]").val();
+			try {
+				const response = await fetch(`/save_pictures/`, {
+					method: 'POST',
+					headers: {
+						'X-CSRFToken': csrfToken
+					},
+					body: formData
+				});
+				// Erreur
+				if (!response.ok) {
+					const errorResponse = await response.json();
+					let errorMessage = 'Erreur lors du traitement de la requête.';
+					if (errorResponse && errorResponse.message) {
+						errorMessage = errorResponse.message;
+					}
+					$('#pictureError').show();
+					$("#pictureError").text(errorMessage);
+					$("#pictureError").addClass('pictureError');
+					$("#pictureError").css({"background-color": "#fef2f2", "color": "#b91c1c", "border": "1px solid #dc2626"});
+					throw new Error('Erreur réseau ou réponse avec statut échoué.');
 				}
-				console.log("toto");
-				$("#pictureError").text(errorMessage);
-				$("#pictureError").addClass('text-red-700 bg-red-100 border border-red-600 p-2 rounded inline-block');
-				throw new Error('Erreur réseau ou réponse avec statut échoué.');
-			}
-			const result = await response.json();
-			$('#pictureError').removeClass();
-			$("#pictureError").text('Le fichier a bien été envoyé !');
-			$("#pictureError").addClass('text-lime-700 bg-lime-100 border border-lime-500 p-2 rounded inline-block');
-			$("#pictureList").append(`
-				<li>
-					<a href="${result.filename.url}">
-						${result.filename.name}
+
+				// Si tout c'est bien passé
+				$("#progressBar").width('100%');
+				$("#progressNumber").text('100%')
+				const result = await response.json();
+				$('#pictureError').show();
+				$('#pictureError').removeClass();
+				$("#pictureError").text('Le fichier a bien été envoyé !');
+				$("#pictureError").addClass('pictureError');
+				$("#pictureError").css({"background-color": "#ecfccb", "color": "#4d7c0f", "border": "1px solid #84cc16"});
+				const newPictureHtml = `
+				<div>
+					${result.filename.name} <!-- Nom du fichier -->
+					<button class="delete_picture" data-pk="${result.filename.pk}">
+					</button>
+					<a target="_blank" href="${result.filename.url}">
+						<img class="max-h-60 max-w-full rounded-lg" src="${result.filename.url}" alt="${result.filename.name}">
 					</a>
-				</li>
-			`);
-		} catch (error) {
-			console.error("Il y a eu un problème avec l'opération fetch:", error);
+				</div>
+				`;
+				$("#pictureList .grid").append(newPictureHtml);
+				// 	$("#pictureList").append(`
+			// 	<li>
+			// 		<a href="${result.filename.url}">
+			// 			${result.filename.name}
+			// 		</a>
+			// 	</li>
+			// `);
+			} catch (error) {
+				console.error("Il y a eu un problème avec l'opération fetch:", error);
+				$("#progressContainer").hide();
+				$("#progressText").hide();
+			}
+
 		}
 	});
 
